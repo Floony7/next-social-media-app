50058b6b923e1f525ebbbcb33e088eb49447fbc5
Floony7 <fred.lunjevich@stretchsense.com>
2024-01-04 13:27:34 +0000

    Header created and shadcn components installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,34 @@
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import { useSession } from 'next-auth/react';
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
-import Profile from '@/components/ui/profile';
+import Profile from "@/components/ui/profile";
+import Header from "@/components/ui/header";
 export default async function Home() {
   const session = await auth();
 
   return (
-    <div className='w-4/6 mx-auto'>
+    <div className="w-4/6 mx-auto">
+      <Header />
       <form action={actions.signIn}>
-      <Button type="submit" variant="default">Sign In</Button>
+        <Button type="submit" variant="default">
+          Sign In
+        </Button>
       </form>
-      <form className='mt-2' action={actions.signOut}>
-      <Button type="submit" variant="default">Sign Out</Button>
+      <form className="mt-2" action={actions.signOut}>
+        <Button type="submit" variant="default">
+          Sign Out
+        </Button>
       </form>
 
-      
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
 
-      { 
-      session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>
-      }
-
-<Profile />
+      <Profile />
     </div>
-  )
+  );
 }
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+import { Input, Avatar, AvatarFallback, AvatarImage, Button } from "./index";
+
+export default function Header() {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
