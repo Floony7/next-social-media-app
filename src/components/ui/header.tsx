b7e2b52e0f1d84d620f3369f1543ec17712a0c04
Floony7 <fred.lunjevich@stretchsense.com>
2024-01-06 15:51:02 +0000

    Navbar tweaks with avatar and sign in / out

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import * as actions from "@/actions";
 import { Input, Avatar, AvatarFallback, AvatarImage, Button } from "./index";
-import { NavbarMain } from "../navbar-main";
 import { auth } from "@/auth";
 import { ReactNode } from "react";
 
@@ -10,9 +9,14 @@ export default async function Header() {
 
   let authContent: ReactNode;
   if (session?.user) {
-    authContent = <UserAvatar src={session.user.image} />;
+    authContent = (
+      <span className="flex gap-3 align-middle">
+        <UserAvatar src={session.user.image} />
+        <SignOut />
+      </span>
+    );
   } else {
-    authContent = <div>Sign in / Sign Out</div>;
+    authContent = <SignIn />;
   }
 
   return (
@@ -30,19 +34,7 @@ export default async function Header() {
           <NavLink href="/about">Chat</NavLink>
           <NavLink href="/about">Life</NavLink>
           <li>
-            <div className="flex gap-3">
-              {/* <form action={actions.signIn}>
-                        <Button className="bg-cyan-500 duration-300 hover:bg-green-700" type="submit" variant="default">
-                        Sign In
-                        </Button>
-                    </form>
-                    <form action={actions.signOut}>
-                        <Button className="bg-cyan-500 duration-300 hover:bg-green-700" type="submit" variant="default">
-                        Sign Out
-                        </Button>
-                    </form> */}
-              {authContent}
-            </div>
+            <div className="flex gap-3">{authContent}</div>
           </li>
         </ul>
       </div>
@@ -64,5 +56,34 @@ function UserAvatar(props: { src: string | null | undefined }) {
       {props.src ? <AvatarImage src={props.src} /> : null}
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
+  );
+}
+
+function SignIn() {
+  return (
+    <>
+      <form action={actions.signIn}>
+        <Button
+          className="bg-cyan-500 duration-300 hover:bg-green-700"
+          type="submit"
+          variant="default"
+        >
+          Sign In
+        </Button>
+      </form>
+    </>
+  );
+}
+
+function SignOut() {
+  return (
+    <form className="mt-1" action={actions.signOut}>
+      <button
+        className="bg-amber-700 duration-300 hover:bg-green-700 rounded text-sm px-1"
+        type="submit"
+      >
+        Sign Out
+      </button>
+    </form>
   );
 }
